f6fa66f0c85bbe6d7289a93aa332c1d62125a797
Maksim Struchalin <m.struch@mail.ru>
2013-12-06 10:16:03 +0000

    Move the instructions from clean.R to GenABEL-general
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/pkg/GenABEL.data@1452 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- data/clean.R
+++
@@ -1,8 +0,0 @@
-#This file is needed only to clean the package during R CMD build
-	
-
-#adpr <- paste(getwd(), "/../inst/extdata/", sep="")
-
-unlink(paste(getwd(), "/../inst/", sep=""), recursive = T)
-unlink(paste(getwd(), "/../R/", sep=""), recursive = T)
-
